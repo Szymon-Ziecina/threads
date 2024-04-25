ebe4a2298565dfa7006e13bec35039a19400b938
Szymon Zięcina <150486140+Szymon-cloud@users.noreply.github.com>
2024-04-25 18:22:39 +0200

    initial commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,22 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter } from "next/font/google";
+import { PropsWithChildren } from "react";
+
+import "../globals.css";
+
+export const metadata = {
+  Title: "Threads",
+  description: "Threads app copy",
+};
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }: PropsWithChildren) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
